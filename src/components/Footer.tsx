4c994bb8640b86493f92fc48dc28502eb97b0656
anshumancanrock <anshu.1239.as@gmail.com>
2025-06-27 21:39:17 +0530

    gg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,23 +11,23 @@ export default function Footer() {
             <div>
                 <ul className="flex gap-8 items-center dark:text-gray-400 text-sm max-sm:text-xs">
                     <li>
-                        <Link href="https://x.com/fardeen14693425">
+                        <Link href="https://x.com/AloneXBT">
                             Twitter
                         </Link>
                     </li>
 
                     <li className="list-disc">
-                        <Link href="https://fardeen.tech/">
+                        <Link href="https://anshuman.us">
                             Developer
                         </Link>
                     </li>
                     <li className="list-disc">
-                        <Link href="https://github.com/Fardeen26">
+                        <Link href="https://github.com/anshumancanrock">
                             GitHub
                         </Link>
                     </li>
                 </ul>            </div>
-            <div className={`${fontInter} text-xs text-gray-600`}>© 2025 Kikoo.AI from Fardeen</div>
+            <div className={`${fontInter} text-xs text-gray-600`}>© 2025 Kikoo.AI from Anshuman</div>
         </footer>
     )
 }
